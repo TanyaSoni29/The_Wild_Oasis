32b3e7b7e5adbb7ed94693e19d08fe80546e44d9
TanyaSoni29 <tanu139sona@gmail.com>
2025-07-12 00:50:29 +0530

    prefetching in react - query

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PER_PAGE } from "../../utils/constants";
 
 export function useBooking() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   const filterValue = searchParams.get("status");
@@ -22,6 +24,8 @@ export function useBooking() {
   // page
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // query
+
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -30,6 +34,20 @@ export function useBooking() {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // prefetching for this we need queryClient then prefetched query method
+  const pageCount = Math.ceil(count / PER_PAGE);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, bookings, error, count };
 }
@@ -41,3 +59,8 @@ export function useBooking() {
 // and if you want multiple filter like status and total price greater then equal then in filter we can pass array of object and loop over array and attach query on each object present in array basis
 
 // now the first time whenever we open a page that spinner will come so in case of pagination that spinner will not show for that we will prefetched the data now here we have concept of prefetching please consider above
+
+// what is prefetching ?
+// prefetching is the process of fetching data that we know is mandatory to our ui before the rendering of that data to ui in context of page we fetch the next page data before hand before the display of that page data and same for going back to the page.
+
+// alternative to prefetching in pagination we can do infinite queries for infinite scroll with React query this react query built in feature for that please go to docs..
