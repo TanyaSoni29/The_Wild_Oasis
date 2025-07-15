53f4fd9856d1efffdbae191ecf3a47a663d00376
TanyaSoni29 <tanu139sona@gmail.com>
2025-07-16 00:54:29 +0530

    dashboard Layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Stats</div>
+      <div>Today's activity</div>
+      <div>Chart Stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
